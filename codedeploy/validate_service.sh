3fc1e60ff503f6177002d5b2ec12865a0c425ad9
shivek <shivek.sacklecha@curefit.com>
2018-11-14 18:17:35 +0530

    codeDeploy and docker port changes

--- codedeploy/validate_service.sh
+++ codedeploy/validate_service.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
-PORT=80
 
-if [ "$DEPLOYMENT_GROUP_NAME" == "osrm" ]
+if [ "$DEPLOYMENT_GROUP_NAME" == "graphhopper-traffic" ]
 then
-  PORT=5200
+  PORT=5203
 else
-  PORT=5200
+  PORT=5203
 fi
 echo ${PORT}
 
 while true
 do
-  result=$(curl -s -o /dev/null -w "%{http_code}" "http://localhost:5200/route?point=12.980269%2C77.694232&point=12.979178%2C77.69725&type=json&locale=en-GB&vehicle=car&weighting=fastest&elevation=false")
+  result=$(curl -s -o /dev/null -w "%{http_code}" "http://localhost:5203/route?point=12.980269%2C77.694232&point=12.979178%2C77.69725&type=json&locale=en-GB&vehicle=car&weighting=fastest&elevation=false")
   if [ "$result" == "200" ]; then
     break
   fi
