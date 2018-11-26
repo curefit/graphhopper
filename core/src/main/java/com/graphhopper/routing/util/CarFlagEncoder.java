0bb688bd01829576f8b29a5d4b7348805bffd318
shivek <shivek.sacklecha@curefit.com>
2018-11-27 03:37:07 +0530

    changed speed to 15

--- core/src/main/java/com/graphhopper/routing/util/CarFlagEncoder.java
+++ core/src/main/java/com/graphhopper/routing/util/CarFlagEncoder.java
@@ -134,7 +134,7 @@ public class CarFlagEncoder extends AbstractFlagEncoder {
 //        defaultSpeedMap.put("unclassified", 30);
 //        defaultSpeedMap.put("residential", 30);
 
-        int defaultSpeed = 25;
+        int defaultSpeed = 15;
         // autobahn
         defaultSpeedMap.put("motorway", defaultSpeed);
         defaultSpeedMap.put("motorway_link", defaultSpeed);
