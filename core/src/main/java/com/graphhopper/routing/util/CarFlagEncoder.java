14fa2a680b54b38986f776223d7aab00c1d149e9
Soumyajyoti Banerjee <soumyajyoti@CF-LP00043.local>
2018-11-29 23:04:15 +0530

    decreased value

--- core/src/main/java/com/graphhopper/routing/util/CarFlagEncoder.java
+++ core/src/main/java/com/graphhopper/routing/util/CarFlagEncoder.java
@@ -134,7 +134,7 @@ public class CarFlagEncoder extends AbstractFlagEncoder {
 //        defaultSpeedMap.put("unclassified", 30);
 //        defaultSpeedMap.put("residential", 30);
 
-        int defaultSpeed = 11;
+        int defaultSpeed = 14;
         // autobahngit
         defaultSpeedMap.put("motorway", defaultSpeed);
         defaultSpeedMap.put("motorway_link", defaultSpeed);
