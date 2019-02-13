d33566305d95aa0d33df2557d9170ed833065659
Soumyajyoti Banerjee <soumyajyoti@CF-LP00043.local>
2019-02-13 18:46:48 +0530

    changed default speed to 14

--- core/src/main/java/com/graphhopper/routing/util/CarFlagEncoder.java
+++ core/src/main/java/com/graphhopper/routing/util/CarFlagEncoder.java
@@ -134,7 +134,7 @@ public class CarFlagEncoder extends AbstractFlagEncoder {
 //        defaultSpeedMap.put("unclassified", 30);
 //        defaultSpeedMap.put("residential", 30);
 
-        int defaultSpeed = 8;
+        int defaultSpeed = 14;
         // autobahngit
         defaultSpeedMap.put("motorway", defaultSpeed);
         defaultSpeedMap.put("motorway_link", defaultSpeed);
