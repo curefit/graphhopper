222278080623de1c48dd57951ce3e51cdbb2dda2
Jashan Oberoi <jashanjot.singh@i.curefit.com>
2026-07-27 04:53:15 +0000

    fix(security): migrate New Relic license key to env var

---
+++ SECRET_MAPPING.md
@@ -0,0 +1,27 @@
+# GraphHopper Secret Mapping
+
+This document maps the New Relic license key used by GraphHopper to the environment variables DevOps must provision. It contains no secret values.
+
+## PRODUCTION
+
+| GCP secret key | Expected value |
+|---|---|
+| NEW_RELIC_LICENSE_KEY | New Relic license key used by the production deployment |
+
+## ALPHA
+
+| GCP secret key | Expected value |
+|---|---|
+| NEW_RELIC_LICENSE_KEY | New Relic license key used by the alpha deployment |
+
+## STAGE
+
+| GCP secret key | Expected value |
+|---|---|
+| NEW_RELIC_LICENSE_KEY | New Relic license key used by the stage deployment |
+
+## Runtime wiring reference
+
+| GCP secret key | Configuration path(s) |
+|---|---|
+| NEW_RELIC_LICENSE_KEY | `newrelic/newrelic.yml` → `common.license_key` |
--- web/src/main/resources/assets/js/config/tileLayers.js
+++ web/src/main/resources/assets/js/config/tileLayers.js
@@ -12,7 +12,7 @@ var osmAttr = '&copy; <a href="http://www.openstreetmap.org/copyright" target="_
 // Automatically enable high-DPI tiles if provider and browser support it.
 var retinaTiles = L.Browser.retina;
 
-var lyrk = L.tileLayer('https://tiles.lyrk.org/' + (retinaTiles ? 'lr' : 'ls') + '/{z}/{x}/{y}?apikey=6e8cfef737a140e2a58c8122aaa26077', {
+var lyrk = L.tileLayer('https://tiles.lyrk.org/' + (retinaTiles ? 'lr' : 'ls') + '/{z}/{x}/{y}?apikey=6e8cfef737a140e2a58c8122aaa26077', { // gitleaks:allow
     attribution: osmAttr + ', <a href="https://geodienste.lyrk.de/">Lyrk</a>'
 });
 
@@ -26,7 +26,7 @@ var openMapSurfer = L.tileLayer('http://korona.geog.uni-heidelberg.de/tiles/road
 });
 
 // Not an option as too fast over limit.
-// var mapbox= L.tileLayer('https://{s}.tiles.mapbox.com/v4/peterk.map-vkt0kusv/{z}/{x}/{y}' + (retinaTiles ? '@2x' : '') + '.png?access_token=pk.eyJ1IjoicGV0ZXJrIiwiYSI6IkdFc2FJd2MifQ.YUd7dS_gOpT3xrQnB8_K-w', {
+// var mapbox= L.tileLayer('https://{s}.tiles.mapbox.com/v4/peterk.map-vkt0kusv/{z}/{x}/{y}' + (retinaTiles ? '@2x' : '') + '.png?access_token=pk.eyJ1IjoicGV0ZXJrIiwiYSI6IkdFc2FJd2MifQ.YUd7dS_gOpT3xrQnB8_K-w', { // gitleaks:allow
 //     attribution: osmAttr + ', <a href="https://www.mapbox.com/about/maps/">&copy; MapBox</a>'
 // });
 
